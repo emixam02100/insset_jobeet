4668419d1eb1a9379ceea9843528e5071d907cb6
Mido <aagour.mohammed@gmail.com>
2014-10-23 15:22:09 +0200

    Add jobeet project

---
+++ src/Erlem/JobeetBundle/Utils/Jobeet.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Jobeet
+ *
+ * @author loic
+ */
+namespace Erlem\JobeetBundle\Utils;
+class Jobeet {
+    
+    static public function slugify($text)
+    {
+        // replace all non letters or digits by -
+        $text = preg_replace('/\W+/', '-', $text);
+ 
+        // trim and lowercase
+        $text = strtolower(trim($text, '-'));
+ 
+        return $text;
+    }
+}
